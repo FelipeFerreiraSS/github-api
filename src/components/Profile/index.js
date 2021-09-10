084f6727c43a66b3c839fc222ef450eadcb37e70
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2021-09-10 14:54:08 -0300

    estilizando profile

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,19 +1,19 @@
 import React from "react";
-import { Wrapper } from './styled'
+import * as S from './styled'
 
 function Profile() {
     return(
-        <Wrapper>
-            <div>
-            <img src="https://avatars.githubusercontent.com/u/65501165?v=4" alt="perfil"/>
-            <div>
-                <h1>Felipe Ferreira</h1>
-                <Wrapper>
-                    <h3>Username: </h3>
-                    <span>felipeferreirass</span>
-                </Wrapper>
-            </div>
-            <Wrapper>
+        <S.Wrapper>
+            <S.WrapperIMG src="https://avatars.githubusercontent.com/u/65501165?v=4" alt="perfil"/>
+            <S.WrapperInfoUser>
+              <div>
+                  <h1>Felipe Ferreira</h1>
+                  <S.WrapperUserName>
+                      <h3>Username: </h3>
+                      <span>felipeferreirass</span>
+                  </S.WrapperUserName>
+              </div>
+            <S.WrapperStatusCount>
               <div>
                 <h4>Followers</h4>
                 <span>60</span>
@@ -26,9 +26,9 @@ function Profile() {
                 <h4>Followings</h4>
                 <span>60</span>
               </div>
-            </Wrapper>
-            </div>
-        </Wrapper>
+            </S.WrapperStatusCount>
+            </S.WrapperInfoUser>
+        </S.Wrapper>
         
     )
 }
--- src/components/Profile/styled.js
+++ src/components/Profile/styled.js
@@ -1,5 +1,39 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    display: flex
+    display: flex;
+    align-items: flex-start;
+`
+
+export const WrapperInfoUser = styled.div`
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+    justify-content: space-between;
+    height: auto;
+    margin-left: 8px;
+`
+
+export const WrapperStatusCount = styled.div`
+    display: flex;
+    align-items: center;
+
+    div {
+        margin: 10px;
+    }
+`
+
+export const WrapperUserName = styled.div`
+    display: flex;
+    align-items: center;
+
+    span {
+        margin-left: 10px;
+    }
+`
+
+export const WrapperIMG = styled.img`
+    border-radius: 50%;
+    width: 200px;
+    margin: 8px;
 `
